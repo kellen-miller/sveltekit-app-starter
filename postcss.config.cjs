5639d7759b95c8f2e27aa290dc2cdf7fb6b7273a
Kellen <37915888+kellen-miller@users.noreply.github.com>
2022-08-03 13:37:18 -0400

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,9 @@
 const postcssImport = require("postcss-import")
 const normalize = require("postcss-normalize")
 const importExtGlob = require("postcss-import-ext-glob")
+const postcssPresetEnv = require("postcss-preset-env");
 const mixins = require("postcss-mixins")
 const nested = require("postcss-nested");
-
-const postcssPresetEnv = require("postcss-preset-env");
 const cssnano = require("cssnano");
 
 const config = {
